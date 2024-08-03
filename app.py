2f841fbaa2d6cd7caf399d182de0d3d7ae236526
joaoc <joaocapitaneo@gmail.com>
2024-08-02 21:50:02 -0300

    codigo mudado para mysql e heroku host

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import mariadb
+import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 
@@ -18,8 +18,13 @@ db_name = 'formulario_db'
 def register():
     # Conexão com o banco de dados
     try:
-        connection = mariadb.connect(host=db_host, user=db_user, password=db_password, database=db_name)
-    except mariadb.Error as e:
+        connection = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+    except mysql.connector.Error as e:
         return jsonify({"message": "Erro ao conectar com o banco de dados", "error": str(e)}), 500
 
     try:
@@ -27,14 +32,14 @@ def register():
 
         data = request.get_json()
         nome = data.get('nome')
-        senha  = data.get('senha')
+        senha = data.get('senha')
         cpf = data.get('cpf')
 
         # Verificar se o usuário já existe
         try:
-            cursor.execute('SELECT * FROM usuarios WHERE nome = ?', (nome,))
+            cursor.execute('SELECT * FROM usuarios WHERE nome = %s', (nome,))
             usuario_existente = cursor.fetchone()
-        except mariadb.Error as e:
+        except mysql.connector.Error as e:
             return jsonify({"message": "Erro ao verificar existência de usuário", "error": str(e)}), 500
 
         if usuario_existente:
@@ -45,10 +50,10 @@ def register():
 
         # Inserir novo usuário no banco de dados
         try:
-            sql = "INSERT INTO usuarios (nome, cpf, senha) VALUES (?, ?, ?)"
+            sql = "INSERT INTO usuarios (nome, cpf, senha) VALUES (%s, %s, %s)"
             cursor.execute(sql, (nome, cpf, hashed_password,))
             connection.commit()
-        except mariadb.Error as e:
+        except mysql.connector.Error as e:
             return jsonify({"message": "Erro ao registrar usuário", "error": str(e)}), 500
 
         return jsonify({"message": "Usuário registrado com sucesso"}), 201
@@ -72,8 +77,13 @@ def login():
 
     # Conexão com o banco de dados
     try:
-        connection = mariadb.connect(host=db_host, user=db_user, password=db_password, database=db_name)
-    except mariadb.Error as e:
+        connection = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+    except mysql.connector.Error as e:
         return jsonify({"message": "Erro ao conectar com o banco de dados", "error": str(e)}), 500
 
     try:
@@ -83,7 +93,7 @@ def login():
         try:
             cursor.execute('SELECT * FROM usuarios')
             usuarios = cursor.fetchall()
-        except mariadb.Error as e:
+        except mysql.connector.Error as e:
             return jsonify({"message": "Erro ao executar consulta no banco de dados", "error": str(e)}), 500
 
         usuario_descoberto = None
@@ -109,4 +119,3 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
